17f1172ef65e8a88f041c78dd6ef3b3586637bee
Vladimir Pravotorov <jtone028@gmail.com>
2020-09-11 17:23:53 +0300

    Added page login

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Switch, Route, } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-function App() {
+// =========== Components =========== //
+const Login = lazy(() => import ('./pages/login/Login'));
+// const Registration = lazy(() => import ('./components/Registration'));
+// const Home = lazy(() => import ('./components/Home/Home'));
+// const Profile = lazy(() => import ('./components/Profile/Profile'));
+// const Task = lazy(() => import ("./components/TaskList/Task";));
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3005/graphql',
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ApolloProvider client={client}>
+          <Suspense fallback={<div>Loading</div>}>
+            <BrowserRouter>
+              <Switch>
+                {/*<Route exact path="/" component={Home} />*/}
+                <Route path="/" component={Login} />
+                {/*<Route path="/registration" component={Registration} />*/}
+                {/*<Route path="/profile" component={Profile} />*/}
+                {/*<Route path="/taskList" component={Task} />*/}
+              </Switch>
+            </BrowserRouter>
+          </Suspense>
+      </ApolloProvider>
   );
 }
 
---
+++ app/src/pages/login/Login.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect, useContext } from 'react'
+
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+import Logo from '../../images/logo.png'
+
+
+
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        media: {
+            paddingTop: '100%', // 16:9
+        },
+        paper: {
+            padding: theme.spacing(2),
+            textAlign: 'center',
+            color: theme.palette.text.secondary,
+        },
+        field: {
+            '& > *': {
+                margin: theme.spacing(1),
+                width: '25ch',
+            },
+        }
+    }),
+);
+
+const Login = () => {
+
+  const classes = useStyles();
+
+  return (
+      <div className={classes.root}>
+          <Grid container spacing={3} justify = "center">
+              <Grid item xs={4}>
+                  <Paper className={classes.paper}>
+
+                      <CardMedia
+                          className={classes.media}
+                          image={Logo}
+                          title="Paella dish"
+                      />
+                      <Typography gutterBottom variant="h6" component="h2">
+                          Exceed-team
+                      </Typography>
+                      <Divider variant="middle" />
+
+                          <form className={classes.field} noValidate autoComplete="off">
+                              <TextField
+                                  required
+                                  id="outlined-required"
+                                  label="Login"
+                                  variant="outlined"
+                              />
+                              <TextField
+                                  required
+                                  id="outlined-required"
+                                  label="Password"
+                                  variant="outlined"
+                                />
+                          </form>
+
+                      <Divider variant="middle" />
+                      <Button variant="contained" color="primary">
+                          Sign IN
+                      </Button>
+                  </Paper>
+
+              </Grid>
+
+          </Grid>
+      </div>
+  )
+
+}
+
+export default Login
